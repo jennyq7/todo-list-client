8f8fe13df6fabbcb3091a78c1c40a52d6768dc47
codingisfun <61808280+jennyq7@users.noreply.github.com>
2023-05-21 22:28:37 -0400

    added styling

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -4,7 +4,7 @@ function HomePage () {
 
 return (
     <main className="main">
-        <h1>Todo List App</h1>
+        <h1 className="main__title">Todo List App</h1>
         <div className="main__buttons">
             <button>Sign Up!</button>
             <button>Log In</button>
